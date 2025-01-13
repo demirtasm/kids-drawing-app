e5c3ec64fea486eeee7d945a9bf63745e80245b8
mukaddesdemirtas <mukaddes.demirtas@softtech.com.tr>
2025-01-13 11:19:00 +0300

    first push

---
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,87 @@
+package com.example.kidsdrawingapp
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var mDrawPath: CustomPath? = null //The path the user is currently drawing on.
+    private var mCanvasBitmap: Bitmap? = null //The bitmap from which the drawings are made.
+    private var mDrawPaint: Paint? = null //Holds the brush properties (color, style, thickness) used for drawing.
+    private var mCanvasPaint: Paint? = null //The paint used to draw the bitmap.
+    private var mBrushSize: Float = 0.toFloat() //Defines the brush thickness.
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null //The canvas on which the drawings will be made.
+    private val mPaths= ArrayList<CustomPath>() //A list that stores all paths (lines) drawn by the user
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE // Line style
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND //Rounded line ends
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat() // Default brush size
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+
+        }
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> { //Called when the user touches the screen.
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+
+            }
+            MotionEvent.ACTION_MOVE->{ //Called when the user moves their finger on the screen
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP->{//Called when the user lifts their finger off the screen. The line is completed and added to the mPaths list.
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()// Redraws the view.
+
+        return true
+    }
+}
+
+
